b2aa9d26d9c8579819481e0cff86420dc5f4c2ae
mumin-ahmod <mumin.ahmed.ledp@gmail.com>
2023-03-28 23:55:02 +0600

    initial commit

---
+++ auth.php
@@ -0,0 +1,65 @@
+<?php
+
+
+
+// CHECK SESSION OF EXISTING LOGIN-------------
+
+$username = null;
+$password = null;
+$username_err = $password_err = $login_err = "";
+
+
+
+
+if($_SERVER['REQUEST_METHOD']== "POST"){
+
+
+    if(empty(trim($_POST["username"]))){ // CHECK IF USERNAME IS EMPTY
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+
+    if(empty(trim($_POST["password"]))){ // CHECKS IF PASSWORD IS EMPTY
+        $password_err = "Please enter password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+
+
+
+
+    if(isset($username) && isset($password)){
+    
+        
+
+        header("location: welcome.html.php");
+
+
+
+    }else{
+    
+    
+        header('WWW-Authenticate: Basic realm="Restricted Area"');
+     header('location: login-auth.html.php');
+    //  die("Please enter your username and password");
+    
+        
+    
+    }
+
+}else
+{
+
+    
+    header('WWW-Authenticate: Basic realm="Restricted Area"');
+    header('location: login-auth.html.php');
+
+}
+
+
+
+
+
+
+?>
---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+
+$hn = 'localhost';
+$db = 'bookrecords';
+$un = 'root';
+$pw = '';
+
+?>
